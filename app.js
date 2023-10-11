c80357ddace1c2321686ca100c7448ac7097cc31
johlbaum <julien.ohlbaum@gmail.com>
2023-10-11 18:18:20 +0200

    MongoDB connection

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 module.exports = app;
