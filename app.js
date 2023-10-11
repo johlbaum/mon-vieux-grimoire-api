eacb6929b4f6037ed4815a616560c98b977e5873
johlbaum <julien.ohlbaum@gmail.com>
2023-10-11 19:12:59 +0200

    Authentication system

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 
 require('dotenv').config();
+
+const userRoutes = require('./routes/user');
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -11,5 +12,10 @@ mongoose
   })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
