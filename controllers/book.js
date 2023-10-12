3dc57a6b7c00492a85095839437a72371fb56a6a
johlbaum <julien.ohlbaum@gmail.com>
2023-10-12 15:55:39 +0200

    Get one book + get best rating

--- controllers/book.js
+++ controllers/book.js
@@ -25,3 +25,19 @@ exports.createBook = (req, res) => {
       res.status(400).json({ error });
     });
 };
+
+exports.getOneBook = (req, res) => {
+  Book.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+exports.getBestRating = (req, res) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then((books) => res.status(200).json(books))
+    .catch((error) => {
+      res.status(404).json({ error });
+    });
+};
--- routes/book.js
+++ routes/book.js
@@ -6,5 +6,7 @@ const bookCtrl = require('../controllers/book');
 
 router.get('/books', bookCtrl.getAllBooks);
 router.post('/books', auth, multer, bookCtrl.createBook);
+router.get('/books/bestrating', bookCtrl.getBestRating);
+router.get('/books/:id', bookCtrl.getOneBook);
 
 module.exports = router;
