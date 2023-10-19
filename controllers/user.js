e4dfea08af4f76a30a1bbd601a700b2418b8c7ec
johlbaum <julien.ohlbaum@gmail.com>
2023-10-19 17:34:28 +0200

    Translation of error messages into English

--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,7 @@ exports.signup = (req, res) => {
       });
       user
         .save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .then(() => res.status(201).json({ message: 'user created' }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
@@ -22,13 +22,13 @@ exports.login = (req, res) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        return res.status(401).json({ error: 'user not found' });
       }
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            return res.status(401).json({ error: 'incorrect password' });
           }
           res.status(200).json({
             userId: user._id,
