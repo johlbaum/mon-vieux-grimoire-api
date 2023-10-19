2707d340288bf2efe37c24c1a18101641c06f57b
johlbaum <julien.ohlbaum@gmail.com>
2023-10-19 17:45:13 +0200

    Adding the secret key to the .env file

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+require('dotenv').config();
 
 exports.signup = (req, res) => {
   bcrypt
@@ -32,7 +33,7 @@ exports.login = (req, res) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+            token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
               expiresIn: '24h',
             }),
           });
