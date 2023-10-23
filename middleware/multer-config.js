ea76989f77e08239a5bdd1266d815af56b18c88a
johlbaum <julien.ohlbaum@gmail.com>
2023-10-23 18:49:54 +0200

    Adding an 'images' folder if it doesn't exist

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,10 +1,16 @@
 const multer = require('multer');
 const sharp = require('sharp');
+const fs = require('fs'); 
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single('image');
 
 const processImage = (req, res, next) => {
+  const imagesDir = 'images';
+  if (!fs.existsSync(imagesDir)) {
+    fs.mkdirSync(imagesDir);
+  }
+
   if (req.file !== undefined) {
     const name = req.file.originalname
       .split('.')
@@ -17,7 +23,7 @@ const processImage = (req, res, next) => {
     sharp(req.file.buffer)
       .webp({ quality: 20 })
       .resize(800)
-      .toFile(`images/${newFileName}`, () => {
+      .toFile(`${imagesDir}/${newFileName}`, () => {
         req.file.filename = newFileName;
         next();
       });
